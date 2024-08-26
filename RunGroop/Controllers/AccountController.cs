7b14700c1382b2284133f8472c4ca539125d9b1b
Rorisang Putu <rorisangputu@gmail.com>
2024-08-26 13:41:37 +0200

    creating login method

--- RunGroop/Controllers/AccountController.cs
+++ RunGroop/Controllers/AccountController.cs
@@ -1,13 +1,56 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using RunGroop.Data;
+using RunGroop.Models;
+using RunGroop.ViewModels;
 
 namespace RunGroop.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
         // GET: AccountController
+        [HttpGet]
         public ActionResult Login()
         {
-            return View();
+            var response = new LoginViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+            var user = await _userManager.FindByEmailAsync(loginVM.Email);
+
+            if (user != null)
+            {
+                //User is found, check password
+                var passwordCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passwordCheck)
+                { //Password is correct, sign in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                //Password is incorrect
+                TempData["Error"] = "Wrong credentials. Please Try again";
+                return View(loginVM);
+            }
+            //User not found
+            TempData["Error"] = "Wrong credentials. Please Try again";
+            return View(loginVM);
         }
 
     }
---
+++ RunGroop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using CloudinaryDotNet.Actions;
+
+namespace RunGroop.ViewModels;
+
+public class LoginViewModel
+{
+    [Display(Name = "Email Address")]
+    [Required(ErrorMessage = "Email Address is required")]
+    public string Email { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+}
