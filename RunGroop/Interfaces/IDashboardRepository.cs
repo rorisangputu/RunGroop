958056cfcdb5a0d6cc0f5fc6eec19f2cc267b9d8
Rorisang Putu <rorisangputu@gmail.com>
2024-08-31 20:46:07 +0200

    creating dashboard service/repo interface

--- RunGroop/Interfaces/IDashboardRepository.cs
+++ RunGroop/Interfaces/IDashboardRepository.cs
@@ -1,8 +1,11 @@
 using System;
+using RunGroop.Models;
 
 namespace RunGroop.Interfaces;
 
 public interface IDashboardRepository
 {
+    Task<List<Race>> GetAllUserRaces();
+    Task<List<Club>> GetAllUserClubs();
 
 }
