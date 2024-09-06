74ea5d1cb968528769220239b52810bda3792c07
Rorisang Putu <rorisangputu@gmail.com>
2024-09-06 14:59:41 +0200

    creating User controller

---
+++ RunGroop/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RunGroop.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: UserController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
