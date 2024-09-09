ef5657ca1b143be0ea1ac79522739f18dc2eee84
Rorisang Putu <rorisangputu@gmail.com>
2024-09-09 05:24:37 +0200

    implemented method in repo

--- RunGroop/Repository/DashboardRepository.cs
+++ RunGroop/Repository/DashboardRepository.cs
@@ -28,4 +28,10 @@ public class DashboardRepository : IDashboardRepository
         var userRaces = _context.Races.Where(r => r.AppUser.Id == curUser);
         return userRaces.ToList();
     }
+
+
+    public async Task<AppUser> GetUserById(string id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
 }
