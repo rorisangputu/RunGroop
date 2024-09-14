de1b02b31643bf53a2a252e38b88c45e73e6e78a
Rorisang Putu <rorisangputu@gmail.com>
2024-09-15 00:05:55 +0200

    added no tracking method to interface

--- RunGroop/Interfaces/IDashboardRepository.cs
+++ RunGroop/Interfaces/IDashboardRepository.cs
@@ -8,5 +8,6 @@ public interface IDashboardRepository
     Task<List<Race>> GetAllUserRaces();
     Task<List<Club>> GetAllUserClubs();
     Task<AppUser> GetUserById(string id);
+    Task<AppUser> GetUserByIdNoTracking(string id);
 
 }
