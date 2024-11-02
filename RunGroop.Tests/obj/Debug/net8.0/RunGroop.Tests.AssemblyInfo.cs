04b73636e52b49b62dabfe4d14366a94b806e5ca
Rorisang Putu <rorisangputu@gmail.com>
2024-11-02 17:50:47 +0200

    addinh nuget packages

--- RunGroop.Tests/obj/Debug/net8.0/RunGroop.Tests.AssemblyInfo.cs
+++ RunGroop.Tests/obj/Debug/net8.0/RunGroop.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunGroop.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b56f66db78ac78c7ad6c76fee1a1cbf30bd0ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2ddbc10d8fabb5d8ffcbb243e25418a2b7ba15a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunGroop.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunGroop.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
